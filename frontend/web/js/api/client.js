94f47af4b5573f1c8def29cdff82d9e1e7426bcd
Celin Villars <villarscelin@gmail.com>
2026-04-04 11:21:16 +0200

    update Frontend to Raspi

--- frontend/web/js/api/client.js
+++ frontend/web/js/api/client.js
@@ -1,5 +1,6 @@
+const API_BASE_URL = "http://localhost:8000"; //Mit Front- und Backend auf Raspi
 //const API_BASE_URL = "http://192.168.1.111:8000"; //Home
-const API_BASE_URL = "http://192.168.210.49:8000"; 
+//const API_BASE_URL = "http://192.168.210.49:8000"; 
 
 
 export async function apiRequest(path, options = {}) {
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-
+API: http://192.168.1.111:8000
+Swagger Docu: http://192.168.1.111:8000/docs
+Frontend: http://192.168.1.111:5500
+Player Page: http://192.168.1.111:5500/players.html
